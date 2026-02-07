8e685a7f9065f7ff3aead524be88e1c18561d6df
sinchana258 <sinchanat826@gmail.com>
2026-02-07 14:23:44 +0530

    changed the cra url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: process.env.REACT_APP_API_BASE_URL,
+
     headers: {
         "Content-Type": "application/json",
     },
